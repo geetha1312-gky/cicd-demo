9607f0af23d8571045fc533a8d436064ef9dec78
Geetha <geethareddy1308@gmail.com>
2026-09-07 22:01:02 +0530

    Initial commit

---
+++ src/main/java/com/example/cicddemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.cicddemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+	public class HelloController {
+
+	    @GetMapping("/hello")
+	    public String hello() {
+
+	        return "Hello, CI/CD World!";
+	    }
+
+}
